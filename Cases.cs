9cfa68cd4e7dc2b16982f0d375005b72fd88ec72
paivH <paiv@paiv.co.uk>
2020-04-01 23:30:12 +0200

    Add files via upload
    Corona app

---
+++ Cases.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corona
+{
+    class Cases
+    {
+   
+        public int cases { get; set; }
+       
+        public int deaths { get; set; }
+  
+        public int recovered { get; set; }
+       
+        public long updated { get; set; }
+        
+        public int active { get; set; }
+       
+        public int affectedCountries { get; set; }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Corona
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+  
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GetCases();
+        }
+        private async void GetCases()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync("https://corona.lmao.ninja/all");
+
+            // var cCases = JsonConvert.DeserializeObject<List<Cases>>(response);
+            JsonClass obj = JsonConvert.DeserializeObject<JsonClass>(json);
+            MessageType messageType = obj.Message;
+            CasesListView.ItemsSource = Items;
+        
+        
+        }
+    }
+}
